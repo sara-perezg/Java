139a53586be2870f4df5e20c45a12190c8336d99
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-05-19 21:05:30 +0200

    My own exception

--- Exceptions/src/com/myExceptions/Main.java
+++ Exceptions/src/com/myExceptions/Main.java
@@ -1,5 +1,24 @@
 package com.myExceptions;
 
+class exceptionIndex extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8404255934429495478L;
+
+	public exceptionIndex() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public exceptionIndex(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
+
+
 public class Main {
 
 	public static void main(String[] args) {
