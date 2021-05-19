7ea84b6d39217d84fdc80aa30cb63ce0eace21a1
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-05-19 20:37:16 +0200

    First exception

--- Exceptions/src/com/isf/Main.java
+++ Exceptions/src/com/isf/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int i = 0;
+		String cadenas[] = {
+		"Cadena 1",
+		"Cadena 2",
+		"Cadena 3",
+		"Cadena 4"
+		};
 		
+		try {
+			for (i=0; i<=4; i++) System.out.println(cadenas[i]);			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			System.out.println("Error indice fuera de rango");
+		}
 	}
 }
