70e65c7307613876271dacf2c97018ca3a797599
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-05-20 10:44:41 +0200

    size and show added

--- Collections/src/com/isf/Libreria.java
+++ Collections/src/com/isf/Libreria.java
@@ -3,9 +3,20 @@ package com.isf;
 import java.util.Iterator;
 import java.util.LinkedList;
 public class Libreria {
-	static LinkedList<Libro> libreria;
+	LinkedList<Libro> libreria = new LinkedList<Libro>();
 	
+	public Libreria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	
+	public Libreria(LinkedList<Libro> libreria) {
+		super();
+		this.libreria = libreria;
+	}
+
+
 	public void addLibro(Libro libro) {
 		libreria.add(libro);
 	}
@@ -15,13 +26,29 @@ public class Libreria {
 	}
 	
 	public void removeLibroByISBN(String isbn, LinkedList<Libro> libreria) {
-		
 		Iterator<Libro> iterator = libreria.iterator();
 		while (iterator.hasNext()) {
 			Libro libro = (Libro) iterator.next();
 			if (libro.getIsbn().equals(isbn)) {
-				
+				iterator.remove();
 			}
 		}
+	}
+	public void removeLibroByAuthor(String titulo, LinkedList<Libro> libreria) {
+		Iterator<Libro> iterator = libreria.iterator();
+		while (iterator.hasNext()) {
+			Libro libro = (Libro) iterator.next();
+			if (libro.getTitulo().equals(titulo)) {
+				iterator.remove();
+			}
+		}
+	}
+	
+	public void showLibreria() {
+		System.out.println(libreria);
+	}
+	
+	public int size() {
+		return libreria.size();
 	}
 }
