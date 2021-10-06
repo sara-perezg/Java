9cf6112cbf39205177e9da95e2145dbcc6f58628
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-10-06 19:39:57 +0200

    fichero review

---
+++ Fichero/src/com/isf/Alumno.java
@@ -0,0 +1,74 @@
+package com.isf;
+
+public class Alumno {
+	public String nombre;
+	public int dni;
+	public double nota;
+	public Alumno() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Alumno(String nombre, int dni, double nota) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.nota = nota;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + dni;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumno other = (Alumno) obj;
+		if (dni != other.dni)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", dni=" + dni + ", nota=" + nota + "]";
+	}
+	
+	
+	
+	
+}
