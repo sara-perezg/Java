ed43f528d14fc07e8820485b5388e74808eb484f
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-10-18 20:34:14 +0200

    runtime and exec

--- Notepad/src/Main.java
+++ Notepad/src/Main.java
@@ -1,7 +1,15 @@
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Runtime rs = Runtime.getRuntime();
+		try {
+			rs.exec("notepad");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 	}
 }
