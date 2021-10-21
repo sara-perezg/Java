71a47b3add5bbb33df1e0f0e3a4dda1caba4333c
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-10-21 18:35:04 +0200

    inicio ejercico 7

---
+++ Fichero/src/com/isf/Ejercicio7/Main.java
@@ -0,0 +1,17 @@
+package com.isf.Ejercicio7;
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String superCarpeta = "curso";
+		String asig1Carpeta = superCarpeta + "/asig1";
+		String asig2Carpeta = superCarpeta + "/asig2";
+		
+		File carpeta = new File();
+		
+		
+	}
+
+}
